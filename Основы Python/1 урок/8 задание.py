b52ab28ef9ad5fcb991e0f959fbc76deadabf4ef
ModenovaAnastasia <fanriall@mail.ru>
2025-06-02 16:57:18 +0500

    initial commit

---
+++ Основы Python/1 урок/8 задание.py
@@ -0,0 +1,8 @@
+'''
+Все готово к тому, чтобы вести дела. У нас есть два списка tasks_my и tasks_friend.
+Теперь необходимо объединить эти списки в один (любым способом, сначала tasks_my, потом tasks_friend) и сохраните результат в tasks_all.
+NB! Переменные tasks_my и tasks_friend уже загружены в ЛМС, создавать их не нужно.
+'''
+
+tasks_all = tasks_my + tasks_friend
+print(tasks_all)
